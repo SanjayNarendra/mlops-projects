040dc012798561633aefac202bf7c51e5e1ce5b3
SanjayNarendra <narendra.sanjay96@gmail.com>
2025-06-29 23:55:34 +0200

    cyber-security: MlFlow and Dagshub intergrated

--- NetworkSecurity/networksecurity/components/model_trainer.py
+++ NetworkSecurity/networksecurity/components/model_trainer.py
@@ -21,7 +21,12 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
+
 from urllib.parse import urlparse
+import mlflow
+
+import dagshub
+dagshub.init(repo_owner='sanjaynarendra', repo_name='mlops-projects', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -30,6 +35,22 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+
+
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            # mlflow.sklearn.log_model(best_model,"model")
+
+            if "dagshub" not in mlflow.get_tracking_uri():
+                mlflow.sklearn.log_model(best_model, "model")
+
 
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
@@ -82,9 +103,14 @@ class ModelTrainer:
 
         classification_train_metric = get_classification_score(y_true=y_train,y_pred=y_train_pred)
 
+        # Track the experiements with mlflow
+        self.track_mlflow(best_model,classification_train_metric)
+
         # prediction on test data
         y_test_pred = best_model.predict(x_test)
         classification_test_metric = get_classification_score(y_true=y_test,y_pred=y_test_pred)
+        
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
@@ -93,6 +119,7 @@ class ModelTrainer:
 
         Network_Model = NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        
         # model pusher
         save_object("final_model/model.pkl",best_model)
         
